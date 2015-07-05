4ede96a661534430ac138ca82c4334545c705b56
Lenfree Yeung <lenfree.yeung@team.telstra.com>
2015-07-05 20:51:25 +1000

    List records

--- route53.go
+++ route53.go
@@ -26,10 +26,34 @@ func getHostedZones(ctx *cli.Context){
   }
 
   for _, hostedZone := range resp.HostedZones {
-    logger.Debug(awsutil.StringValue(hostedZone.Name))
     logger.WithFields(logrus.Fields{
       "RR set count": awsutil.StringValue(hostedZone.ResourceRecordSetCount),
       "Zone ID": awsutil.StringValue(hostedZone.ID),
     }).Info("Name", awsutil.StringValue(hostedZone.Name))
+    getRR(hostedZone.ID)
+  }
+}
+
+func getRR(hostedZoneID *string) {
+  svc := route53.New(nil)
+  rrsresp, err := svc.ListResourceRecordSets (& route53.ListResourceRecordSetsInput {HostedZoneID: hostedZoneID})
+  if err != nil {
+     logger.Fatal(err)
+  }
+
+  for i := range rrsresp.ResourceRecordSets {
+    for j := range rrsresp.ResourceRecordSets[i].ResourceRecords{
+      rrset := map[string]string{
+        "name":  awsutil.StringValue(*rrsresp.ResourceRecordSets[i].Name),
+        "type":  awsutil.StringValue(*rrsresp.ResourceRecordSets[i].Type),
+        "ttl":  awsutil.StringValue(*rrsresp.ResourceRecordSets[i].TTL),
+        "value": awsutil.StringValue(rrsresp.ResourceRecordSets[i].ResourceRecords[j].Value),
+      }
+    logger.WithFields(logrus.Fields{
+      "type":  rrset["type"],
+      "value": rrset["value"],
+      "TTL": rrset["ttl"],
+    }).Info("Name", rrset["name"])
+    }
   }
 }
